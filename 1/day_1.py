e901b0cc462c02ff4f127f53e31f9ded214c374b
ThibaultRobine <thibault.robine@telecom-sudparis.eu>
2025-09-20 15:48:51 +0200

    First day completed

--- 1/day_1.py
+++ 1/day_1.py
@@ -4,11 +4,11 @@ import numpy as np
 with open("input.txt") as f:
     tab = np.sort(np.array([line.split() for line in f], int).T, axis=1)
 
-print(np.sum(np.abs(tab[0]-tab[1]))) #result
+print(np.sum(np.abs(tab[0]-tab[1]))) #result (2580760)
 
 #part 2
 temp = 0
 for i in tab[0]:
     temp += i * np.sum([j==i for j in tab[1]])
 
-print(temp) #result
+print(temp) #result (25358365)
